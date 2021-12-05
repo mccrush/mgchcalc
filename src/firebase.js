a2a9a9d90a2e2e95a94f0fe7dc1a7b3da20a2695
mccrush <mccrush2027@gmail.com>
2021-12-05 17:25:20 +0700

    Add Firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import apiKey from '../apiKey'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey,
+  authDomain: "mgch-calc.firebaseapp.com",
+  projectId: "mgch-calc",
+  storageBucket: "mgch-calc.appspot.com",
+  messagingSenderId: "25810727339",
+  appId: "1:25810727339:web:7f290561e8c4eeed19e2ee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
