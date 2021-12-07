32324d722c751584a12669c3ed2ab88e181e4af6
mccrush <mccrush2027@gmail.com>
2021-12-07 17:12:15 +0700

    Create module

---
+++ src/components/calc/CalcSiryo.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="row pt-2 pb-2 border">
+    <!---->
+    <div class="col-3 pe-0">
+      Группа сырья<br />
+      <select
+        class="form-select form-select-sm w-100"
+        aria-label="Select group resourse"
+        @change="selectCatSiryo"
+        v-model="categorySiryoId"
+      >
+        <option v-for="catS in categorySiryos" :key="catS.id" :value="catS.id">
+          {{ catS.title }}
+        </option>
+      </select>
+    </div>
+
+    <div class="col-3 pe-0">
+      Сырье<br />
+      <select
+        class="form-select form-select-sm w-100"
+        aria-label="Select group resourse"
+        @change="selectSiryo"
+        v-model="siryoSelectId"
+      >
+        <option v-for="sir in siryos" :key="sir.id" :value="sir.id">
+          {{ sir.title }}
+        </option>
+      </select>
+    </div>
+
+    <div class="col-2 text-center pe-0">
+      Ед. / стоимость<br />
+      <small> </small>
+    </div>
+    <div class="col-2 pe-0">
+      Размеры<br />
+      <input
+        type="text"
+        class="form-control form-control-sm"
+        v-model="siryoSize"
+      />
+    </div>
+    <div class="col-2 text-center pe-0">
+      Сумма<br />
+      {{ siryoSumma }}
+    </div>
+    <!---->
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    ClacSiryo
+  },
+  data() {
+    return {
+      categorySiryoId: '',
+      siryoSelectId: '',
+      siryoSize: ''
+    }
+  },
+  computed: {
+    categorySiryos() {
+      return this.$store.getters.group
+    },
+    siryos() {
+      return this.$store.getters.siryo.filter(
+        item => item.categoryId === this.categorySiryoId
+      )
+    },
+    siryo() {
+      return (
+        this.$store.getters.siryo.find(
+          item => item.id === this.siryoSelectId
+        ) || { ed: 'кв.м.', price: 0 }
+      )
+    },
+    siryoSumma() {
+      let size = this.siryoSize
+      size = size.replace(/,/g, '.')
+      return Math.ceil(this.siryo.price * size)
+    }
+  },
+  methods: {
+    selectCatSiryo() {
+      this.siryoSelectId = ''
+      this.siryoSize = ''
+    }
+  }
+}
+</script>
--- src/views/Calc.vue
+++ src/views/Calc.vue
@@ -16,86 +16,35 @@
       </div>
     </div>
   </div>
+  <CalcSiryo v-if="siryoEnable" @siryo-price="siryoPrice" />
   <div class="row pt-2 pb-2 border">
-    <!---->
-    <div class="col-3 pe-0">
-      Группа сырья<br />
-      <select
-        class="form-select form-select-sm w-100"
-        aria-label="Select group resourse"
-        @change="selectCatSiryo"
-        v-model="categorySiryoId"
-      >
-        <option v-for="catS in categorySiryos" :key="catS.id" :value="catS.id">
-          {{ catS.title }}
-        </option>
-      </select>
+    <div class="col-12 d-flex justify-content-between">
+      <div></div>
+      <div>Итого: {{ sumItogo }}</div>
     </div>
-
-    <div class="col-3 pe-0">
-      Сырье<br />
-      <select
-        class="form-select form-select-sm w-100"
-        aria-label="Select group resourse"
-        @change="selectSiryo"
-        v-model="siryoSelectId"
-      >
-        <option v-for="sir in siryos" :key="sir.id" :value="sir.id">
-          {{ sir.title }}
-        </option>
-      </select>
-    </div>
-
-    <div class="col-2 text-center pe-0">
-      Ед. / стоимость<br />
-      <small> {{ siryo.ed }} / {{ siryo.price }}</small>
-    </div>
-    <div class="col-2 pe-0">
-      Размеры<br />
-      <input
-        type="text"
-        class="form-control form-control-sm"
-        v-model="siryoSize"
-      />
-    </div>
-    <div class="col-2 text-center pe-0">
-      Сумма<br />
-      {{ siryoSumma }}
-    </div>
-    <!---->
   </div>
 </template>
 
 <script>
+import CalcSiryo from '@/components/calc/CalcSiryo'
 export default {
+  components: {
+    CalcSiryo
+  },
   data() {
     return {
       siryoEnable: true,
-      categorySiryoId: '',
-      siryoSelectId: '',
-      siryoSize: ''
+      siryoSum: 0
     }
   },
   computed: {
-    categorySiryos() {
-      return this.$store.getters.group
-    },
-    siryos() {
-      return this.$store.getters.siryo.filter(
-        item => item.categoryId === this.categorySiryoId
-      )
-    },
-    siryo() {
-      return (
-        this.$store.getters.siryo.find(
-          item => item.id === this.siryoSelectId
-        ) || { ed: 'кв.м.', price: 0 }
-      )
-    },
-    siryoSumma() {
-      let size = this.siryoSize
-      size = size.replace(/,/g, '.')
-      return Math.ceil(this.siryo.price * size)
+    sumItogo() {
+      return this.siryoSum
+    }
+  },
+  methods: {
+    siryoPrice(sum) {
+      this.siryoSum = sum
     }
   }
 }
