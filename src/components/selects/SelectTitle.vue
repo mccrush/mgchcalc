12c8c482bf6f2fa05a7e5f1c5d3a8f1e76c737f3
mccrush <mccrush2027@gmail.com>
2021-12-10 13:42:13 +0700

    Create component

---
+++ src/components/selects/SelectTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <select
+    class="form-select form-select-sm w-100"
+    aria-label="Select group resourse"
+  >
+    <option v-for="elem in array" :key="elem.id" :value="elem.id">
+      {{ elem.title }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: ['array']
+}
+</script>
