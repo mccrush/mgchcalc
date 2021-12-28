e41f60569477c5ec4cf4906f4686ce9d734f5af4
mccrush <mccrush2027@gmail.com>
2021-12-28 12:13:10 +0700

    Update razdel names

--- src/data/razdels.js
+++ src/data/razdels.js
@@ -1,7 +1,7 @@
 export default [
-  { id: '1', title: 'Раздел Группы сырья', alias: 'group' },
-  { id: '2', title: 'Раздел Сырье', alias: 'siryo' },
-  { id: '3', title: 'Раздел Тип фрезеровки', alias: 'frezer' },
-  { id: '4', title: 'Раздел Работы', alias: 'rabota' },
+  { id: '1', title: 'Раздел Группы материалов', alias: 'group' },
+  { id: '2', title: 'Раздел Материалы', alias: 'siryo' },
+  { id: '3', title: 'Раздел Вид обработки', alias: 'frezer' },
+  { id: '4', title: 'Раздел Толщина материала', alias: 'rabota' },
   { id: '5', title: 'Раздел Дополнительные услуги', alias: 'dopuslug' }
 ]
