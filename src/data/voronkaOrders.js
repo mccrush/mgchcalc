23f858c681657c84b68d7e7e9e402e9260ba1266
mccrush <mccrush2027@gmail.com>
2022-01-05 22:29:26 +0700

    Raname status alias

--- src/data/voronkaOrders.js
+++ src/data/voronkaOrders.js
@@ -1,9 +1,9 @@
 export default [
-  { id: '1', title: 'Новый запрос', pos: 1, alias: 'new' },
+  { id: '1', title: 'Новый запрос', pos: 1, alias: 'neworder' },
   { id: '2', title: 'Отправлен расчет', pos: 2, alias: 'raschet' },
   { id: '3', title: 'Передан в обработку', pos: 3, alias: 'nafrezer' },
   { id: '4', title: 'Готов к отгрузке', pos: 4, alias: 'ready' },
-  { id: '5', title: 'Передан заказчику', pos: 5, alias: 'done' },
+  { id: '5', title: 'Передан заказчику', pos: 5, alias: 'doneorder' },
   { id: '6', title: 'Успешно реализован', pos: 6, alias: 'success' },
-  { id: '7', title: 'Не реализован', pos: 7, alias: 'fail' },
+  { id: '7', title: 'Не реализован', pos: 7, alias: 'failorder' },
 ]
