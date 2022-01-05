bcaca2c0e80b383e504ac60efaeb0b6adcf0e309
mccrush <mccrush2027@gmail.com>
2022-01-05 10:43:12 +0700

    Add etaps in Order component

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,37 +1,19 @@
 <template>
   <div>
     <div class="row pt-2 ps-2 pe-2 pb-3">
-      <div v-for="etap in voronkaObrabotka" :key="etap.id" class="col-4">
+      <div v-for="etap in voronkaOrders" :key="etap.id" class="col-4">
         <OrderList
           :title="etap.title"
           :array="getArray(etap.alias)"
           @edit-order="editOrder"
         />
       </div>
-
-      <!-- <div class="col-4">
-        <OrderList title="Новые" :array="ordersNew" @edit-order="editOrder" />
-      </div>
-      <div class="col-4">
-        <OrderList
-          title="В работе"
-          :array="ordersProgress"
-          @edit-order="editOrder"
-        />
-      </div>
-      <div class="col-4">
-        <OrderList
-          title="Выполненные"
-          :array="ordersDone"
-          @edit-order="editOrder"
-        />
-      </div> -->
     </div>
   </div>
 </template>
 
 <script>
-import voronkaObrabotka from '@/data/voronkaObrabotka'
+import voronkaOrders from '@/data/voronkaOrders'
 import OrderList from '@/components/orders/OrderList'
 
 export default {
@@ -40,30 +22,18 @@ export default {
   },
   data() {
     return {
-      voronkaObrabotka
+      voronkaOrders
     }
   },
-  // beforeCreate() {
-  //   this.$store.dispatch('getItems', { type: 'order' })
-  // },
   computed: {
     orders() {
       return this.$store.getters.order
-    },
-    getArray(alias) {
-      return this.orders.filter(item => item.status === alias)
     }
-    // ordersNew() {
-    //   return this.orders.filter(item => item.status === 'new')
-    // },
-    // ordersProgress() {
-    //   return this.orders.filter(item => item.status === 'inprogress')
-    // },
-    // ordersDone() {
-    //   return this.orders.filter(item => item.status === 'done')
-    // }
   },
   methods: {
+    getArray(alias) {
+      return this.orders.filter(item => item.status === alias)
+    },
     editOrder(order) {
       this.$emit('edit-order', { order, mod: 'edit' })
     }
