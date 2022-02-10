a8f8b42e23b0f521fc41fdf2713d53d857d3b383
mccrush <mccrush2027@gmail.com>
2022-02-10 13:22:59 +0700

    Add drag methods

--- src/components/admin/FormMaterialVid.vue
+++ src/components/admin/FormMaterialVid.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="col-12 pt-2">
+  <div
+    class="col-12 dropzone pt-2"
+    @drop="onDrop($event)"
+    @dragover.prevent
+    @dragleave.prevent
+    @dragenter="onDragEnter($event)"
+  >
     <div
       v-for="(item, index) in sortFilter"
       :key="item.id"
       class="row mt-1 pt-2 pb-2"
       :class="{ 'bg-light': index % 2 === 0 }"
+      draggable="true"
+      @dragstart.stop="onDragStart($event, item)"
     >
       <div class="drag-button ms-1 p-0 pb-1">
         <ButtonDrag />
@@ -63,6 +71,40 @@ export default {
     }
   },
   methods: {
+    onDragStart(e, item) {
+      console.log('DragStart, item:', item.id)
+      e.dataTransfer.dropEffect = 'move'
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.setData('itemId', item.id)
+    },
+    onDragEnter(e) {
+      if (e.target.classList.contains('dropzone')) {
+        // let dropzones = document.querySelectorAll('.dropzone')
+        // dropzones.forEach(item => {
+        //   item.lastChild.style.paddingTop = '0'
+        // })
+        // e.target.lastChild.style.paddingTop = '64px'
+      }
+    },
+    onDrop(e, status) {
+      //console.log('onDrop, status:', status)
+
+      // let dropzone = document.querySelector(
+      //   '.dropzone[data-status ="' + status + '" ]'
+      // )
+      // dropzone.lastChild.style.paddingTop = '0'
+
+      let itemId = e.dataTransfer.getData('itemId')
+      console.log('onDrop, itemId:', itemId)
+      // this.items.forEach(item => {
+      //   if (item.id === itemId) {
+      //     if (item.status !== status) {
+      //       item.status = status
+      //       this.$store.dispatch('updateItemStatus', { item })
+      //     }
+      //   }
+      // })
+    },
     saveItem(item) {
       this.$emit('save-item', { item })
     }
