1d4a1bf9bba549049c3e4d9e4ad507f8439e417c
mccrush <mccrush2027@gmail.com>
2022-02-14 10:39:20 +0700

    Create new admin components structure

--- src/scripts/createItem.js
+++ src/scripts/createItem.js
@@ -5,70 +5,86 @@ import Obrabotkavid from '@/classes/obrabotkavid'
 import Obrabotkatolshina from '@/classes/obrabotkatolshina'
 import Dopuslug from '@/classes/dopuslug'
 import Customer from '@/classes/customer'
-export default (type, categoryId = '') => {
-  const id = Date.now().toString()
-  const title = '[]'
 
-  if (type === 'materialvid') {
-    return {
-      id,
-      title,
-      type: 'materialvid',
-      position: 0
-    }
-  } else if (type === 'obrabotkavid') {
-    return {
-      id,
-      title,
-      type: 'obrabotkavid',
-      position: 0
-    }
-  } else if (type === 'dopuslug') {
-    return {
-      id,
-      title,
-      type: 'dopuslug',
-      alias: '',
-      ed: '',
-      price: 0,
-      position: 0
-    }
-  } else if (type === 'material') {
-    return {
-      id,
-      title,
-      categoryId,
-      categoryTitle: '',
-      ed: '',
-      price: 0,
-      ostatok: 0,
-      type: 'material',
-      position: 0
-    }
-  } else if (type === 'obrabotkatolshina') {
-    return {
-      id,
-      title,
-      categoryId,
-      categoryTitle: '',
-      priceS: 0,
-      priceM: 0,
-      priceL: 0,
-      type: 'obrabotkatolshina',
-      position: 0
-    }
-  } else if (type === 'customer') {
-    return {
-      id,
-      title,
-      address: '',
-      inn: '',
-      kpp: '',
-      ogrn: '',
-      bic: '',
-      rs: '',
-      ks: '',
-      type: 'customer'
-    }
+export default (type, categoryId) => {
+  switch (type) {
+    case 'materialvid':
+      return new Materialvid()
+    case 'material':
+      return new Material(categoryId)
+    case 'obrabotkavid':
+      return new Obrabotkavid()
+    case 'obrabotkatolshina':
+      return new Obrabotkatolshina(categoryId)
+    case 'dopuslug':
+      return new Dopuslug()
+    case 'customer':
+      return new Customer()
   }
+
+  // const id = Date.now().toString()
+  // const title = '[]'
+
+  // if (type === 'materialvid') {
+  //   return {
+  //     id,
+  //     title,
+  //     type: 'materialvid',
+  //     position: 0
+  //   }
+  // } else if (type === 'obrabotkavid') {
+  //   return {
+  //     id,
+  //     title,
+  //     type: 'obrabotkavid',
+  //     position: 0
+  //   }
+  // } else if (type === 'dopuslug') {
+  //   return {
+  //     id,
+  //     title,
+  //     type: 'dopuslug',
+  //     alias: '',
+  //     ed: '',
+  //     price: 0,
+  //     position: 0
+  //   }
+  // } else if (type === 'material') {
+  //   return {
+  //     id,
+  //     title,
+  //     categoryId,
+  //     categoryTitle: '',
+  //     ed: '',
+  //     price: 0,
+  //     ostatok: 0,
+  //     type: 'material',
+  //     position: 0
+  //   }
+  // } else if (type === 'obrabotkatolshina') {
+  //   return {
+  //     id,
+  //     title,
+  //     categoryId,
+  //     categoryTitle: '',
+  //     priceS: 0,
+  //     priceM: 0,
+  //     priceL: 0,
+  //     type: 'obrabotkatolshina',
+  //     position: 0
+  //   }
+  // } else if (type === 'customer') {
+  //   return {
+  //     id,
+  //     title,
+  //     address: '',
+  //     inn: '',
+  //     kpp: '',
+  //     ogrn: '',
+  //     bic: '',
+  //     rs: '',
+  //     ks: '',
+  //     type: 'customer'
+  //   }
+  // }
 }
---
+++ src/views/Admin2.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <AdminHeader
+      v-model:razdel="razdel"
+      v-model:categoryId="categoryId"
+      v-model:searchText="searchText"
+      v-model:sortType="sortType"
+      @add-item="addItem"
+    />
+    <AdminForms
+      :razdel="razdel"
+      :categoryId="categoryId"
+      :searchText="searchText"
+      :sortType="sortType"
+    />
+    <AdminFooter />
+  </div>
+</template>
+
+<script>
+import createItem from '@/scripts/createItem'
+
+import AdminHeader from '@/components/admin/AdminHeader'
+import AdminForms from '@/components/admin/AdminForms'
+import AdminFooter from '@/components/admin/AdminFooter'
+
+export default {
+  components: {
+    AdminHeader,
+    AdminForms,
+    AdminFooter
+  },
+  data() {
+    return {
+      razdel: localStorage.getItem('cl-razdel') || 'materialvid',
+      categoryId: localStorage.getItem('cl-categoryId') || '',
+      searchText: '',
+      sortType: 'asc'
+    }
+  },
+  methods: {
+    addItem() {
+      const item = createItem(this.razdel, this.categoryId)
+      console.log('new item:', item)
+      //this.$store.dispatch('addItem', { item })
+    }
+  }
+}
+</script>
