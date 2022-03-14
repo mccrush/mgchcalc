d2cce642da7b7686e6b808a822f3a268a78bc5ff
mccrush <mccrush2027@gmail.com>
2022-03-14 19:16:06 +0700

    Update menu name

--- src/components/interface/SidebarOffcanvas.vue
+++ src/components/interface/SidebarOffcanvas.vue
@@ -174,7 +174,41 @@
               d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
             />
           </svg>
-          <span class="ms-3">Сотрудники</span>
+          <span class="small ms-3">Пользователи</span>
+        </button>
+      </li>
+      <li class="nav-item">
+        <button
+          v-if="userId === 'rNl0ejcTSIhMycVqx4XhTqVVhAv1'"
+          class="
+            btn-sidebar
+            text-start
+            d-flex
+            align-items-center
+            btn
+            w-100
+            pt-2
+            pb-2
+          "
+          :class="{ 'btn-sidebar-active': pathname === 'backup' }"
+          @click="setPathName('backup')"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-download"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"
+            />
+            <path
+              d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"
+            />
+          </svg>
+          <span class="ms-3">Бэкап</span>
         </button>
       </li>
     </ul>
