9e912ec34b5e54c17b7492a6ff3f4b6ca2c8b6a7
mccrush <mccrush2027@gmail.com>
2022-05-23 17:07:45 +0700

    Add remove button for Postavshik

--- src/components/material/TabPostavshiki.vue
+++ src/components/material/TabPostavshiki.vue
@@ -3,7 +3,14 @@
     <div class="col-4 mb-2 pe-0">
       <ButtonAdd class="w-100" @click="addNewPostavshik" />
     </div>
-    <div class="col-8 mb-2"></div>
+    <div class="col-4 mb-2"></div>
+    <div class="col-4 mb-2">
+      <ButtonTrash
+        v-if="item"
+        class="w-100 lh-sm p-2"
+        @click="removePostavshik"
+      />
+    </div>
     <div class="col-4 pe-0"><ListPostavshiki :postavshiki="postavshiki" /></div>
     <div class="col-8">
       <FormPostavshik v-if="item" :item="item" @save-item="saveItem" />
@@ -15,12 +22,14 @@
 import Postavshik from './../../classes/postavshikClass'
 
 import ButtonAdd from './../elements/buttons/ButtonAdd.vue'
+import ButtonTrash from './../elements/buttons/ButtonTrash.vue'
 import ListPostavshiki from './ListPostavshiki.vue'
 import FormPostavshik from './FormPostavshik.vue'
 
 export default {
   components: {
     ButtonAdd,
+    ButtonTrash,
     ListPostavshiki,
     FormPostavshik
   },
@@ -41,6 +50,18 @@ export default {
     addNewPostavshik() {
       const item = Object.assign({}, new Postavshik())
       this.$store.dispatch('addItem', { item })
+    },
+    removePostavshik() {
+      if (
+        confirm(
+          'Точно удалить поставщика? Это действие невозможно будет отменить'
+        )
+      ) {
+        this.$store.dispatch('removeItem', {
+          type: this.item.type,
+          id: this.item.id
+        })
+      }
     },
     saveItem() {
       this.$store.dispatch('updateItem', { item: this.item })
