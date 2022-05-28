fcbe34f6cf9a9313e08bf4388e722e978da78a4c
mccrush <mccrush2027@gmail.com>
2022-05-28 16:54:38 +0700

    Add default value

--- src/classes/contactClass.js
+++ src/classes/contactClass.js
@@ -1,13 +1,13 @@
 import getId from './../scripts/getId'
 
 export default class Contact {
-  constructor(title = 'Новый контакт') {
+  constructor(title = 'Новый контакт', contactName = 'Новый контакт') {
     this.id = getId()
     this.title = title
     this.type = 'contact'
     this.description = ''
     this.position = 1
-    this.contactName = ''
+    this.contactName = contactName
     this.contactNickname = ''
     this.contacts = []
     this.companyId = ''
