5bd5958fb02266a3a8151bd452d21e192b4509d9
mccrush <mccrush2027@gmail.com>
2022-06-06 07:26:10 +0700

    Settings form Obrabotkatolshina

--- src/components/admin_new/FormObrabotkatolshina.vue
+++ src/components/admin_new/FormObrabotkatolshina.vue
@@ -28,6 +28,67 @@
       </form>
     </div>
 
+    <!-- Вид обработки -->
+    <div class="col-3 mt-2 pe-1">
+      <form @submit.prevent class="form-floating">
+        <select
+          class="form-select"
+          id="inputVidobrabotki"
+          v-model="item.categoryId"
+          @change="$emit('save-item')"
+        >
+          <option
+            v-for="obrabotkavid in obrabotkavids"
+            :key="obrabotkavid.id"
+            :value="obrabotkavid.id"
+          >
+            {{ obrabotkavid.title }}
+          </option>
+        </select>
+        <label for="inputVidobrabotki">Вид обработки</label>
+      </form>
+    </div>
+
+    <!-- Стоимости -->
+    <div class="col-3 mt-2 ps-1 pe-1">
+      <form @submit.prevent class="form-floating">
+        <input
+          type="number"
+          class="form-control form-control-sm"
+          id="inputS"
+          v-model.number="item.priceS"
+          @blur="$emit('save-item')"
+        />
+        <label for="inputS">До 100</label>
+      </form>
+    </div>
+
+    <div class="col-3 mt-2 ps-1 pe-1">
+      <form @submit.prevent class="form-floating">
+        <input
+          type="number"
+          class="form-control form-control-sm"
+          id="inputM"
+          v-model.number="item.priceM"
+          @blur="$emit('save-item')"
+        />
+        <label for="inputM">От 101 до 1000</label>
+      </form>
+    </div>
+
+    <div class="col-3 mt-2 pe-1">
+      <form @submit.prevent class="form-floating">
+        <input
+          type="number"
+          class="form-control form-control-sm"
+          id="inputL"
+          v-model.number="item.priceL"
+          @blur="$emit('save-item')"
+        />
+        <label for="inputL">От 1001</label>
+      </form>
+    </div>
+
     <!-- Комментарий -->
     <div class="col-12 mt-2">
       <form class="form-floating">
